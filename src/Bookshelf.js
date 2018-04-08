7d5da1cc129d245a81c8cffc423d6b1308a522cb
Igor333m <igor333m@yahoo.com>
2018-04-07 19:04:50 -0700

    bug: bookshelf none fix

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -18,11 +18,9 @@ class Bookshelf extends Component {
 	*/
 	handleChange = (book, event) => {
 		event.preventDefault()
-		if (event.target.value === "none") {
-			this.props.onRemoveBook(book)
-		}else {
+		
 			this.props.onUpdateBook(book, event.target.value)
-		}
+
 	}
 
 
